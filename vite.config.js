16dc6dd90decfdab78858e7c8519d396c9a10c69
Sanjay Bhandari <bhandarisanjay879@gmail.com>
2023-11-26 00:41:53 +1000

    changed config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'build', // Set the output directory to 'build'
+  }
 });
